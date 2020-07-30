ac67a9a47b29afbdced89a882e86d438762fb93e
Anderson Pinheiro <andersonfernandespinheiro@gmail.com>
2020-07-30 17:29:03 -0300

    Commit Aula2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assers/img/Logo.png';
-import '../Menu/Menu.css'
-import Button from '../Button'
-
-//import ButtonLink from './components/ButtonLink';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src ={Logo} alt="Aluraflix logo"></img>
-            </a>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-            <Button className="ButtonLink" href="/">
-                Novo vídeo 
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
